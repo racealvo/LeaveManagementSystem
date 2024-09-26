b86829faeb20ef7945d97f6cd77209e5693ef365
Ray Salvo <salvo.raymond@gmail.com>
2024-09-26 10:38:39 -0400

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "John Doe",
+                //DateOfBirth = new DateTime(1990, 1, 1)
+            };
+
+            return View(data);
+        }
+    }
+}
